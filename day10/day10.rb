5a27dc8b9806d75aeb7540800c4f4af76f5d9a00
Andy Gonçalves <agoncalves1@gmail.com>
2024-12-10 14:52:38 +0000

    feat: add day 10, part 2

--- day10/day10.rb
+++ day10/day10.rb
@@ -16,13 +16,27 @@ module Day10
     puts trailhead_score_sum
   end
 
+  def self.part2
+    matrix = File.foreach('day10/input.txt').map { |line| line.strip.chars.map(&:to_i) }
+
+    trailhead_score_sum = 0
+
+    matrix.each_with_index do |r, row|
+      r.each_with_index do |_, col|
+        trailhead_score_sum += trailhead_score_rating(matrix, row, col)
+      end
+    end
+
+    puts trailhead_score_sum
+  end
+
   def self.trailhead_score(matrix, row, col)
     return 0 if matrix[row][col] != 0
 
-    dfs_score(deep_copy_matrix(matrix), row, col)
+    dfs_score_once(deep_copy_matrix(matrix), row, col)
   end
 
-  def self.dfs_score(matrix, row, col)
+  def self.dfs_score_once(matrix, row, col)
     height = matrix[row][col]
 
     if height == 9
@@ -31,10 +45,29 @@ module Day10
       return 1
     end
 
-    up = row > 0 && matrix[row - 1][col] == height + 1 ?                    dfs_score(matrix, row - 1, col) : 0
-    down = row < matrix.length - 1 && matrix[row + 1][col] == height + 1 ?  dfs_score(matrix, row + 1, col) : 0
-    right = col < matrix.length - 1 && matrix[row][col + 1] == height + 1 ? dfs_score(matrix, row, col + 1) : 0
-    left = col > 0 && matrix[row][col - 1] == height + 1 ?                  dfs_score(matrix, row, col - 1) : 0
+    up = row > 0 && matrix[row - 1][col] == height + 1 ?                    dfs_score_once(matrix, row - 1, col) : 0
+    down = row < matrix.length - 1 && matrix[row + 1][col] == height + 1 ?  dfs_score_once(matrix, row + 1, col) : 0
+    right = col < matrix.length - 1 && matrix[row][col + 1] == height + 1 ? dfs_score_once(matrix, row, col + 1) : 0
+    left = col > 0 && matrix[row][col - 1] == height + 1 ?                  dfs_score_once(matrix, row, col - 1) : 0
+
+    down + right + up + left
+  end
+
+  def self.trailhead_score_rating(matrix, row, col)
+    return 0 if matrix[row][col] != 0
+
+    dfs_score_all(matrix, row, col)
+  end
+
+  def self.dfs_score_all(matrix, row, col)
+    height = matrix[row][col]
+
+    return 1 if height == 9
+
+    up = row > 0 && matrix[row - 1][col] == height + 1 ?                    dfs_score_all(matrix, row - 1, col) : 0
+    down = row < matrix.length - 1 && matrix[row + 1][col] == height + 1 ?  dfs_score_all(matrix, row + 1, col) : 0
+    right = col < matrix.length - 1 && matrix[row][col + 1] == height + 1 ? dfs_score_all(matrix, row, col + 1) : 0
+    left = col > 0 && matrix[row][col - 1] == height + 1 ?                  dfs_score_all(matrix, row, col - 1) : 0
 
     down + right + up + left
   end
@@ -44,6 +77,8 @@ module Day10
   end
 
   private_class_method(:trailhead_score)
-  private_class_method(:dfs_score)
+  private_class_method(:trailhead_score_rating)
+  private_class_method(:dfs_score_once)
+  private_class_method(:dfs_score_all)
   private_class_method(:deep_copy_matrix)
 end
--- main.rb
+++ main.rb
@@ -5,6 +5,8 @@ require_relative('day10/day10')
 case ARGV[0]
 when '1001'
   Day10.part1
+when '1002'
+  Day10.part2
 else
   puts('Please choose a day and part to run, in the format DDPP.')
 end
