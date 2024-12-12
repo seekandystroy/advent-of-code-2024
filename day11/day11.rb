da93e4478b1408739e585583f7875b4fb083bc52
Andy Gonçalves <agoncalves1@gmail.com>
2024-12-12 13:28:55 +0000

    feat: wip day 11, part 2

--- day11/day11.rb
+++ day11/day11.rb
@@ -10,13 +10,20 @@ module Day11
     puts(nums.length)
   end
 
+  def self.part2
+    nums = File.new('day11/input.txt').readline(chomp: true).split(' ').map(&:to_i).sort
+
+    nums.each { |num| }
+    puts(nums.length)
+  end
+
   def self.blink(nums)
     nums.flat_map { |num| individual_blink(num) }
   end
 
   def self.individual_blink(num)
     if num == 0
-      1
+      [1]
     elsif even_digits?(num)
       half_zeroes = 10.pow(num_digits(num) / 2)
       first_half = num / half_zeroes
@@ -24,7 +31,7 @@ module Day11
 
       [first_half, second_half]
     else
-      num * 2024
+      [num * 2024]
     end
   end
 
@@ -34,6 +41,17 @@ module Day11
 
   def self.even_digits?(num)
     num_digits(num).even?
+  end
+
+  def self.blink_with_cache(num, cache, blink_num, path = Set.new)
+    return cache[num][blink_num] if cache.dig(num, blink_num)
+
+    children = individual_blink(num)
+    cache[num] = cache[num] || []
+    cache[num].append(children.length)
+    children.map do |child|
+      blink_with_cache(child, cache, blink_num + 1, path.add(num))
+    end.sum
   end
 
   private_class_method(:blink)
