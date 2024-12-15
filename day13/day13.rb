3f26cfbd401633a50c77970acf694ccdee526687
Andy Gonçalves <agoncalves1@gmail.com>
2024-12-15 01:17:21 +0000

    refactor: clean up module
    1. extract reading method for legibility
    2. remove method for calculating tokens
    3. set private methods

--- day13/day13.rb
+++ day13/day13.rb
@@ -7,28 +7,34 @@ module Day13
 
     sum = 0
     until file.eof?
-      button_a = file.readline(chomp: true)
-      a_matches = button_a.match(/Button A: X\+(\d+), Y\+(\d+)/)
-      a_x = a_matches[1].to_i
-      a_y = a_matches[2].to_i
-
-      button_b = file.readline(chomp: true)
-      b_matches = button_b.match(/Button B: X\+(\d+), Y\+(\d+)/)
-      b_x = b_matches[1].to_i
-      b_y = b_matches[2].to_i
-
-      prize = file.readline(chomp: true)
-      prize_matches = prize.match(/Prize: X=(\d+), Y=(\d+)/)
-      prize_x = prize_matches[1].to_i
-      prize_y = prize_matches[2].to_i
-
+      prize_x, prize_y, a_x, a_y, b_x, b_y = read_prize_lines(file)
       a_presses, b_presses = amount_of_presses(prize_x, prize_y, a_x, a_y, b_x, b_y)
-      sum += tokens_to_spend(a_presses, b_presses)
 
-      file.readline unless file.eof?
+      sum += a_presses * 3 + b_presses
     end
 
     puts sum
+  end
+
+  def self.read_prize_lines(file)
+    button_a = file.readline(chomp: true)
+    a_matches = button_a.match(/Button A: X\+(\d+), Y\+(\d+)/)
+    a_x = a_matches[1].to_i
+    a_y = a_matches[2].to_i
+
+    button_b = file.readline(chomp: true)
+    b_matches = button_b.match(/Button B: X\+(\d+), Y\+(\d+)/)
+    b_x = b_matches[1].to_i
+    b_y = b_matches[2].to_i
+
+    prize = file.readline(chomp: true)
+    prize_matches = prize.match(/Prize: X=(\d+), Y=(\d+)/)
+    prize_x = prize_matches[1].to_i
+    prize_y = prize_matches[2].to_i
+
+    file.readline unless file.eof?
+
+    [prize_x, prize_y, a_x, a_y, b_x, b_y]
   end
 
   def self.amount_of_presses(prize_x, prize_y, a_x, a_y, b_x, b_y)
@@ -44,7 +50,6 @@ module Day13
     [0, 0]
   end
 
-  def self.tokens_to_spend(a_presses, b_presses)
-    a_presses * 3 + b_presses
-  end
+  private_class_method(:read_prize_lines)
+  private_class_method(:amount_of_presses)
 end
